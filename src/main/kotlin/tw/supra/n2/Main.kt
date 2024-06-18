2f628c8b8543d276a0a73c424e680c0cad5caf0d
Supra.W <supra.cn@gmail.com>
2024-06-18 19:55:30 +0800

    适配kotlin2

--- src/main/kotlin/tw/supra/n2/Main.kt
+++ src/main/kotlin/tw/supra/n2/Main.kt
@@ -1,11 +1,12 @@
 package tw.supra.w2
 
 fun main() {
-    println(Pair(1, 2) == Pair(1, 2))
-//    Nature(9).start()
+//    println(Pair(1, 2) == Pair(1, 2))
+    Nature(9).start()
 }
 
 class Position<DimensionType>(dimensions: Set<String>) {
 val dimensions :LinkedHashSet<String> by lazy {  LinkedHashSet(dimensions.sorted())}
     val key by lazy { dimensions }
 }
+
